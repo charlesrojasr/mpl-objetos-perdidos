b19d4a44ae05422e4637347eabc9d4de0d8cb439
charlesrojasr <charles.rojasr@gmail.com>
2026-05-19 16:33:32 -0500

    protege el usuario administrador id= 1

--- m-modulos/usuarios_buscar.php
+++ m-modulos/usuarios_buscar.php
@@ -28,21 +28,45 @@ include 'usuarios_config.php';
         <td><?= $row['username'] ?></td>
         <td><?= $row['role_name'] ?></td>
         <td class="text-center">
-          <input type="checkbox"
-            class="switch-estado"
-            data-id="<?= $row['id'] ?>"
-            <?= $row['estado'] == 1 ? 'checked' : '' ?>>
+
+          <?php if ($row['id'] == 1) { ?>
+
+            <span class="badge badge-dark">
+              PROTEGIDO
+            </span>
+
+          <?php } else { ?>
+
+            <input type="checkbox"
+              class="switch-estado"
+              data-id="<?= $row['id'] ?>"
+              <?= $row['estado'] == 1 ? 'checked' : '' ?>>
+
+          <?php } ?>
+
         </td>
         <td><?= date('Y-m-d', strtotime($row['created_at'] . ' -1 hour')) ?></td>
         <td><?= date('Y-m-d', strtotime($row['updated_at'] . ' -1 hour')) ?></td>
-        
+
         <td class="text-center">
-          <button
-            class="btn btn-warning btn-sm btn-reset-clave"
-            data-id="<?= $row['id'] ?>"
-            data-doc="<?= $row['documento_identidad'] ?>">
-            <i class="fas fa-key"></i>
-          </button>
+
+          <?php if ($row['id'] == 1) { ?>
+
+            <button class="btn btn-secondary btn-sm" disabled>
+              <i class="fas fa-lock"></i>
+            </button>
+
+          <?php } else { ?>
+
+            <button
+              class="btn btn-warning btn-sm btn-reset-clave"
+              data-id="<?= $row['id'] ?>"
+              data-doc="<?= $row['documento_identidad'] ?>">
+              <i class="fas fa-key"></i>
+            </button>
+
+          <?php } ?>
+
         </td>
       </tr>
 
